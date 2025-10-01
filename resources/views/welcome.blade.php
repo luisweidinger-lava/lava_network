9c6bf6e1b2d3ac9ee75b322e242d5dc049a7cdb9
luisweidinger-lava <luis.weidinger@code.berlin>
2025-10-01 20:25:19 +0200

    Primary push of lava_network projetc for MTM03_1 class

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ninja Network</title>
+</head>
+<body>
+<h1>Welcome to the Luis Ninja Network</h1>
+<p>Click the button below to view the list of ninjas.</p>
+
+<a href="" class="btn">
+    Find Ninjas!
+</a>
+</body>
+</html>
