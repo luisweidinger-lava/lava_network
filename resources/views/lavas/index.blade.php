83af3339099b2058787ac604b63e8b51060dcf8a
luisweidinger-lava <luis.weidinger@code.berlin>
2025-10-01 21:27:14 +0200

    Add index page and second Route

---
+++ resources/views/lavas/index.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lava Network | Home</title>
+</head>
+<body>
+<h2>Currently Available Lavas</h2>
+
+<ul>
+    <li>Lavas Here</li>
+</ul>
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 <h1>Welcome to the Luis Ninja Network</h1>
 <p>Click the button below to view the list of ninjas.</p>
 
-<a href="" class="btn">
+<a href="/lavas" class="btn">
     Find Ninjas!
 </a>
 </body>
